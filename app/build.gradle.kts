b33d2d9f2973682a842f8bb3b5bccf668ce34213
Oktawiusz Jeziorek <oki.jeziorek@gmail.com>
2026-03-19 00:41:26 +0100

    Bump versionCode to 3 in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "pl.oki.frostalert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
