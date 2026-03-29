5f3bfb537f129d94f62c7523cc9624dfbd0054b9
Oktawiusz Jeziorek <oki.jeziorek@gmail.com>
2026-03-29 23:10:46 +0200

    v1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "pl.oki.frostalert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2"
+        versionCode = 6
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
