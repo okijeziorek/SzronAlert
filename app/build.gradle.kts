f105a74b5a1f67b086d16333a432968b84b46595
Oktawiusz Jeziorek <oki.jeziorek@gmail.com>
2026-04-08 12:39:29 +0200

    Increment `versionCode` to `8` in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "pl.oki.frostalert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
