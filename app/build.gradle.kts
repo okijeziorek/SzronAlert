fdb736db5e1a95290bf6023ea70ccd1be049be34
Oktawiusz Jeziorek <oki.jeziorek@gmail.com>
2026-04-13 12:12:02 +0200

    Increment `versionCode` to `10` in `app/build.gradle.kts` and apply minor XML cleanup in `.idea/misc.xml`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "pl.oki.frostalert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
